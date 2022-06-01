e072e6219213e96ef0a92a2aa42f62ce02f6bf71
Hosen <hsona.333@gmail.com>
2022-06-01 03:35:52 +0300

    Letters

--- js/signup.js
+++ js/signup.js
@@ -12,55 +12,55 @@ $(document).ready(() => {
 //signUp
 var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
 var english = /^[A-Za-z0-9]*$/;
-//function for userName valdition
-//This fuction first need to check if thi user name already exits in the database
+//function for userName validation
+//This function first need to check if thi user name already exits in the database
 //, then it must be only in english and number no special characters
 function checkUserName() { }
 
-// function for email add valdition
-// this function will first check if this email address already exsits in our database
-//then it must be according to the universal email address valdition
+// function for email add validation
+// this function will first check if this email address already exists in our database
+//then it must be according to the universal email address validation
 function checkEmail() {
 
- }
+}
 
 //function for password validation
-//this function will check id paaword is valid according to our rules
-//our rules is: to be more than 8 characters, only english, atleast one capital character ans one lower,at least one number, no special character
+//this function will check id password is valid according to our rules
+//our rules is: to be more than 8 characters, only english, at least one capital character ans one lower,at least one number, no special character
 function checkPassword() {
   let password = document.getElementById("uPassword");
   let errLabl = document.getElementById("errMsgUp");
-  errLabl.style.color="red";
+  errLabl.style.color = "red";
   if (password.value.length >= 8) {
-    //check if there is atlest on capital letter
+    //check if there is at lest on capital letter
     if (checkCap(password.value, password.value.length) == true) {
       if (checkLow(password.value, password.value.length) == true) {
         if (checkSpecialChar(password.value, password.value.length) == true) {
           if (threeNums(password.value, password.value.length) == true) {
             if (checkEnglish(password.value, password.value.length) == true) {
-              confirmPass(password.value,errLabl);
-              
-            } else errLabl.innerHTML="The password must english characters .";
-          } else errLabl.innerHTML="The password must contain atleast one numbers in row .";
-        } else errLabl.innerHTML="The password must contain special character atleast one.";
-      } else errLabl.innerHTML="The password must contain lower letter atleast one.";
-    } else errLabl.innerHTML="The password must contain capital letter atleast one.";
-  } else errLabl.innerHTML="The password must be minimum 8 letters";
+              confirmPass(password.value, errLabl);
+
+            } else errLabl.innerHTML = "The password must english characters .";
+          } else errLabl.innerHTML = "The password must contain at least one numbers in row .";
+        } else errLabl.innerHTML = "The password must contain special character at least one.";
+      } else errLabl.innerHTML = "The password must contain lower letter at least one.";
+    } else errLabl.innerHTML = "The password must contain capital letter at least one.";
+  } else errLabl.innerHTML = "The password must be minimum 8 letters";
 }
 
 //function for confirmation the password 
-function confirmPass(pass,errLabl){
-  let confPass=document.getElementById("uConfPassowrd");
-  if (pass.localeCompare(confPass.value)!=0)
-    errLabl.innerHTML="The password isn't the same !"
-    saveUser();
+function confirmPass(pass, errLabl) {
+  let confPass = document.getElementById("uConfPassword");
+  if (pass.localeCompare(confPass.value) != 0)
+    errLabl.innerHTML = "The password isn't the same !"
+  saveUser();
 }
 
 //saving the user in the array of objects and local storage 
 
-function saveUser(){
-  
-  
+function saveUser() {
+
+
 }
 
 /* help functions*/
@@ -95,7 +95,7 @@ function checkNum(str, size) {
   if (str[size - 1] >= "0" && str[size - 1] <= "9") return true;
   else return threeNums(str, size - 1);
 }
-//check if it is in enlgish
+//check if it is in english
 function checkEnglish(str, size) {
   if (size - 1 == -1) return true;
   //console.log(str[size-1].match(english));
