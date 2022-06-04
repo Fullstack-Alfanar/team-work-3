b810de73a26dd274fc8087dadabaf45e909b1909
Hosen <hsona.333@gmail.com>
2022-06-04 19:28:17 +0300

    fix issues

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,33 +1,33 @@
 $(document).ready(() => {
+  $("#logoutBtn").click(() => {
+    document.cookie = "Email =" + "" + ";" + "expires=" + ";path=/";
+    window.location.replace("../index.html");
+  });
+  function getUser() {
+    let cookies = document.cookie;
+    let splitCookies = cookies.split("=");
+    return splitCookies[1];
+  }
+  email = getUser();
+  if (email == "") {
+    window.location.replace("../index.html");
+  }
+  getData();
 
-    $("#logoutBtn").click(() => {
-        document.cookie = "Email =" + "" + ";" + "expires=" + ";path=/";
-        window.location.replace("../index.html");
-    })
-    function getUser() {
-        let cookies = document.cookie;
-        let splitCookies = cookies.split("=");
-        return splitCookies[1];
-    }
-    let email = getUser();
-    if (email == "") {
-        window.location.replace("../index.html");
-    }
-    let user;
-    for (let i of usersArray) {
-        if (i.Email == email) {
-            user = i.UserName;
-            break;
-        }
-    }
-    let data = $(`<i class="fa fa-fw fa-user"></i>`);
-    $("#username").text(user).append(data);
-})
+  let data = $(`<i class="fa fa-fw fa-user"></i>`);
+  $("#username").text(user).append(data);
+});
 
-let usersArray = [];
+let user, email;
 //sets data in the array from local storage whenever the page rerun
 function getData() {
-    if (localStorage.getItem("userList"))
-        usersArray = JSON.parse(localStorage.getItem("userList"));
+  let usersArray = [];
+  if (localStorage.getItem("userList"))
+    usersArray = JSON.parse(localStorage.getItem("userList"));
+  for (let i of usersArray) {
+    if (i.Email == email) {
+      user = i.UserName;
+      break;
+    }
+  }
 }
-getData();
